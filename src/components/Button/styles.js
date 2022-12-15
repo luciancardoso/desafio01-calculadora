93a9fb89b57fc1ff83f150362004bb7ee74a09de
Lucian Cardoso <luciancardoso20@gmail.com>
2022-12-14 20:33:56 -0400

    Desafio 01 calculadora todas as funções foram implementadas

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #0000FF;
+    color: #FFFFFF;
+    font: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
